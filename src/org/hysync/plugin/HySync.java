5a772dce2d85a115f2fe9d8fbe2c0fcd51ece84a
Charlie J <37641589+CharlieJoseph@users.noreply.github.com>
2018-07-20 17:14:31 +0100

    Basic setup created

---
+++ src/org/hysync/plugin/HySync.java
@@ -0,0 +1,16 @@
+package org.hysync.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HySync extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
